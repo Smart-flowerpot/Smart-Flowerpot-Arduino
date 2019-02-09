b4e3d55ec793cb9d32534d49fd43ded94cc37b8b
Musa <musaunal680@gmail.com>
2019-02-09 16:31:12 +0300

    Client olarak ayarlamaları yapıldı
    - Tüm sensör verilerinin servera gönderilmesi
    - Serverdan veri okunması
    - projenin genel iskeleti oluşturuldu

--- SonDevre/SonDevre.ino
+++ SonDevre/SonDevre.ino
@@ -3,16 +3,29 @@
 
 const char* ssid = "ETUNET-asistan";
 const char* password = "tobb1234";
-unsigned char status_water_engine = 0;
-WiFiServer server(80); 
-int DHT11_pin=2; // DHT11_pin olarak Dijital 2'yi belirliyoruz.
-int suMotoru=15;
+
+const int plant_id = 1;
+const String plant_name = "menekşe";
+const String plant_type = "aerhurasia";
+int status_water_engine = 0;
+int moisture_air = 0;   // yüzde olarak veriyor
+int moisture_soil = 0;  // 0-1024 arası (yüzdeye çevirildi)
+int temperature_air = 0;  // celcius
+int frostbite = 0;   // çiğ oluşma noktası yüzde
+int light = 0;
+
+const char* host = "omercalisma123.xyz";
+String path_post = "";
+const char* path_get = "/SmartFlowerpot/get_plant.php?id=1";
+
+int DHT11_pin = 2; // DHT11_pin olarak Dijital 2'yi belirliyoruz.
+int suMotoru_pin = 15;
 dht11 DHT11_sensor; // DHT11_sensor adında bir DHT11 nesnesi oluşturduk.
  
 void setup()
 {
   Serial.begin(115200);
-  pinMode(15,OUTPUT);
+  pinMode(suMotoru_pin , OUTPUT);
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -23,83 +36,51 @@ void setup()
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  server.begin();
-  Serial.println("Server started");
-  Serial.println(WiFi.localIP());       //asdasdasdasda
 }
  
 void loop()
 {
-  digitalWrite(15,HIGH);
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
+  digitalWrite(suMotoru_pin , LOW);
+  DHT11_sensor.read(DHT11_pin);
+  moisture_air = (int)DHT11_sensor.humidity;
+  temperature_air = (int)DHT11_sensor.temperature; 
+  frostbite = DHT11_sensor.dewPoint();
+  moisture_soil = analogRead(A0);
+  Serial.println(moisture_air);
+  Serial.println(temperature_air);
+  Serial.println(moisture_soil);
+  Serial.println(frostbite);
   
-  int chk = DHT11_sensor.read(DHT11_pin);
-  
-  Serial.println("new client");
-  while(!client.available())
+  path_post = String("/SmartFlowerpot/update_plant.php?plant_id=")+plant_id+"&light="+light+"&temperature="+temperature_air+"&moisture_air="+moisture_air+"&moisture_soil="+moisture_soil+"&frostbite="+frostbite+"&water="+status_water_engine+"&name="+plant_name+"&type="+plant_type;
+
+  WiFiClient client;
+
+  Serial.printf("\n[Connecting to %s ... ", host);
+  if (client.connect(host, 80))
   {
-    delay(1);
+    Serial.println("connected]");
+
+    Serial.println("[Sending a request]");
+    client.print(String("POST ") + path_post + " HTTP/1.1\r\n" +
+           "Host: " + host + "\r\n" + 
+           "Connection: close\r\n\r\n");
+
+    Serial.println("[Response:]");
+    while (client.connected() || client.available())
+    {
+      if (client.available())
+      {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+      }
+    }
+    client.stop();
+    Serial.println("\n[Disconnected]");
   }
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
-  if (req.indexOf("/EngineOff") != -1)
-  {
-    status_water_engine = 0;   
-    digitalWrite(15 , LOW);
-    Serial.println("Engine OFF");
-  }
-  else if(req.indexOf("/EngineOn") != -1)
-  {
-    status_water_engine = 1;
-    digitalWrite(15 , HIGH);
-    Serial.println("Engine ON");
-  }
-  
-  String web = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-  web += "<html>\r\n";
-  web += "<body>\r\n";
-  web += "<h1>Water Engine Status</h1>\r\n";
-  web += "<p>\r\n";
-  if(status_water_engine == 1)
-      web += "Engine is On\r\n";
   else
-      web += "Engine is Off\r\n";
-  web += "</p>\r\n";
-  web += "</p>\r\n";
-  web += "<a href=\"/EngineOn\">\r\n";
-  web += "<button>Engine On</button >\r\n";
-  web += "</a>\r\n";
-  web += "</p>\r\n";
-  
-  web += "<a href=\"/EngineOff\">\r\n";
-  web += "<button>Engine Off</button >\r\n";
-  web += "</a>\r\n";
-
-  client.println(web);
-  
-  client.println("Nem Orani (%): <br>");
-  client.println((float)DHT11_sensor.humidity, 2);
- 
-  client.println("Sicaklik (Celcius): <br>");
-  client.println((float)DHT11_sensor.temperature, 2);
- 
-  client.println("Sicaklik (Kelvin): <br>");
-  client.println(DHT11_sensor.kelvin(), 2);
-  
-  client.println("Sicaklik (Fahrenheit): <br>");
-  client.println(DHT11_sensor.fahrenheit(), 2);
- 
-  client.println("Cig Olusma Noktasi: <br>");
-  client.println(DHT11_sensor.dewPoint(), 2);
-  client.println("<br>------------------");
-  client.println("<br>------------------<br>");
-  client.println(analogRead(A0)); 
-
-  client.print("</body>\r\n");
-  client.print("</html>\r\n");
-  
+  {
+    Serial.println("connection failed!]");
+    client.stop();
+  }
+  delay(5000);
 }
